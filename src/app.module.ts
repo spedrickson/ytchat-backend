2d870aa1a0f278601f6310995afa74c59325d7e4
spedrickson <spedrickson@gmail.com>
2022-04-06 01:39:34 -0700

    add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MessageModule } from './message/message.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MessageModule],
+  imports: [MessageModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
